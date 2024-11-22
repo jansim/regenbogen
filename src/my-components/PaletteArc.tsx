b78e4118fa73e8d63e71f4330607124fbacafb34
Jan Simson <git@simson.io>
2024-11-22 23:35:17 +0100

    Remove react as an import in Arc
    Interistingly enough, Claude keeps re-adding this

--- src/my-components/PaletteArc.tsx
+++ src/my-components/PaletteArc.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 
 const PaletteArc = ({ width = 50, arcWidth = 10 }) => {
   const [isHovering, setIsHovering] = useState(false);
